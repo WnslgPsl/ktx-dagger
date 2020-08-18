089e66220928a85a9e12008d3fef3f34e7f47128
noah <noah.kim@kakaocorp.com>
2020-08-19 08:44:19 +0900

    ViewModel들을 멀티바인딩으로 관리 하기

---
+++ app/src/main/java/com/babosamo/ktxdagger/di/ViewModelModule.kt
@@ -0,0 +1,28 @@
+package com.babosamo.ktxdagger.di
+
+import androidx.lifecycle.ViewModel
+import com.babosamo.ktxdagger.MainViewModel
+import com.squareup.inject.assisted.dagger2.AssistedModule
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import dagger.multibindings.Multibinds
+
+@AssistedModule
+@Module(includes = [AssistedInject_ViewModelModule::class])
+abstract class ViewModelModule {
+
+    //일반 뷰모델들의 멀티 바인딩
+    @Multibinds
+    abstract fun bindsViewModels(): Map<Class<out ViewModel>, @JvmSuppressWildcards ViewModel>
+
+    //AssistedInject로 관리하는 ViewModel Factory 멀티바인딩
+    @Multibinds
+    abstract fun bindsAssistedViewModels(): Map<Class<out ViewModel>, @JvmSuppressWildcards AssistedSavedStateViewModelFactory<out ViewModel>>
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun bindsMainViewModel(factory: MainViewModel.Factory): AssistedSavedStateViewModelFactory<out ViewModel>
+
+}
