0c3814e1ef93a7f9324aae455d26b83f5adc2ca8
noah <noah.kim@kakaocorp.com>
2020-08-19 08:43:07 +0900

    ViewModel에 AssistedInject를 위한 코드 추가하기

--- app/src/main/java/com/babosamo/ktxdagger/MainViewModel.kt
+++ app/src/main/java/com/babosamo/ktxdagger/MainViewModel.kt
@@ -3,9 +3,11 @@ package com.babosamo.ktxdagger
 import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.babosamo.ktxdagger.di.AssistedSavedStateViewModelFactory
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
 
-class MainViewModel(private val handle: SavedStateHandle) : ViewModel() {
-
+class MainViewModel @AssistedInject constructor(@Assisted private val handle: SavedStateHandle) : ViewModel() {
 
     private var id = handle["id"] ?: 0
         set(value) {
@@ -22,4 +24,8 @@ class MainViewModel(private val handle: SavedStateHandle) : ViewModel() {
         Log.i("MainViewModel", "plus id: $id")
         id += 1
     }
+
+    @AssistedInject.Factory
+    interface Factory : AssistedSavedStateViewModelFactory<MainViewModel>
+
 }
